2dc315c9597ade375b24b8ea068ce42f6e1ed3af
Edouard Rucquoy <erucquoy@gmail.com>
2018-09-25 20:48:15 +0200

    game

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -11,3 +11,23 @@ var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
+
+var geometry = new THREE.Geometry();
+geometry.vertices.push(new THREE.Vector3( -3, 0, 0) );
+geometry.vertices.push(new THREE.Vector3( 0, 3, 0) );
+geometry.vertices.push(new THREE.Vector3( 3, 0, 0) );
+
+var line = new THREE.Line( geometry, material );
+
+scene.add ( line );
+
+var animate = function () {
+    requestAnimationFrame( animate );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+};
+
+animate();
